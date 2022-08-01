a3e81596da85a1e520873482add04bc4c2f5734d
ariefzuhri <ariefzuhri72@gmail.com>
2022-08-01 15:26:25 +0700

    Add cachedIn() when getting the news to persist the data beyond configuration changes

--- app/src/main/java/com/ariefzuhri/discovergoogle/ui/main/MainViewModel.kt
+++ app/src/main/java/com/ariefzuhri/discovergoogle/ui/main/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.ariefzuhri.discovergoogle.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.ariefzuhri.discovergoogle.domain.model.Article
 import com.ariefzuhri.discovergoogle.domain.usecase.NewsUseCase
 import kotlinx.coroutines.flow.Flow
@@ -9,5 +11,5 @@ import kotlinx.coroutines.flow.Flow
 class MainViewModel(useCase: NewsUseCase) : ViewModel() {
 
     val news: Flow<PagingData<Article>> =
-        useCase.getNews()
+        useCase.getNews().cachedIn(viewModelScope)
 }

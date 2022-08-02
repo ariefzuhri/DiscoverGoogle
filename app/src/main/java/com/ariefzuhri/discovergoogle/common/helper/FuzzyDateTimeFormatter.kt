7c44a061c1077d124406932a43ca2dfc7b1477fc
ariefzuhri <ariefzuhri72@gmail.com>
2022-08-02 10:11:29 +0700

    Optimize code for FuzzyDateTimeFormatter

--- app/src/main/java/com/ariefzuhri/discovergoogle/common/helper/FuzzyDateTimeFormatter.kt
+++ app/src/main/java/com/ariefzuhri/discovergoogle/common/helper/FuzzyDateTimeFormatter.kt
@@ -5,11 +5,14 @@
 package com.ariefzuhri.discovergoogle.common.helper
 
 import android.content.Context
+import android.util.Log
 import com.ariefzuhri.discovergoogle.R
 import com.ariefzuhri.discovergoogle.common.util.orZero
 import java.util.*
 
 object FuzzyDateTimeFormatter {
+
+    private val TAG = FuzzyDateTimeFormatter::class.java.simpleName
 
     private const val SECONDS = 1
     private const val MINUTES = 60 * SECONDS
@@ -29,41 +32,58 @@ object FuzzyDateTimeFormatter {
         val beforeSeconds = (date?.time.orZero() / 1000).toInt()
         val nowSeconds = (Calendar.getInstance().timeInMillis / 1000).toInt()
         val timeDifference = nowSeconds - beforeSeconds
-        require(timeDifference >= 0) { "Date must be in the past!" }
 
-        context?.resources?.let { res ->
-            return if (timeDifference < 15 * SECONDS) {
-                res.getString(R.string.fuzzydatetime_now)
-            } else if (timeDifference < MINUTES) {
-                res.getQuantityString(R.plurals.fuzzydatetime_seconds_ago,
-                    timeDifference,
-                    timeDifference)
-            } else if (timeDifference < HOURS) {
-                res.getQuantityString(R.plurals.fuzzydatetime_minutes_ago,
-                    timeDifference / MINUTES,
-                    timeDifference / MINUTES)
-            } else if (timeDifference < DAYS) {
-                res.getQuantityString(R.plurals.fuzzydatetime_hours_ago,
-                    timeDifference / HOURS,
-                    timeDifference / HOURS)
-            } else if (timeDifference < WEEKS) {
-                res.getQuantityString(R.plurals.fuzzydatetime_days_ago,
-                    timeDifference / DAYS,
-                    timeDifference / DAYS)
-            } else if (timeDifference < MONTHS) {
-                res.getQuantityString(R.plurals.fuzzydatetime_weeks_ago,
-                    timeDifference / WEEKS,
-                    timeDifference / WEEKS)
-            } else if (timeDifference < YEARS) {
-                res.getQuantityString(R.plurals.fuzzydatetime_months_ago,
-                    timeDifference / MONTHS,
-                    timeDifference / MONTHS)
-            } else {
-                res.getQuantityString(R.plurals.fuzzydatetime_years_ago,
-                    timeDifference / YEARS,
-                    timeDifference / YEARS)
+        if (timeDifference < 0) {
+            with(date.toString()) {
+                Log.e(TAG, "Date $this must be in the past!")
+                return this
             }
         }
-        return date.toString()
+
+        context?.resources?.let { res ->
+            return when {
+                timeDifference < 15 * SECONDS -> {
+                    res.getString(R.string.fuzzydatetime_now)
+                }
+                timeDifference < MINUTES -> {
+                    res.getQuantityString(R.plurals.fuzzydatetime_seconds_ago,
+                        timeDifference,
+                        timeDifference)
+                }
+                timeDifference < HOURS -> {
+                    res.getQuantityString(R.plurals.fuzzydatetime_minutes_ago,
+                        timeDifference / MINUTES,
+                        timeDifference / MINUTES)
+                }
+                timeDifference < DAYS -> {
+                    res.getQuantityString(R.plurals.fuzzydatetime_hours_ago,
+                        timeDifference / HOURS,
+                        timeDifference / HOURS)
+                }
+                timeDifference < WEEKS -> {
+                    res.getQuantityString(R.plurals.fuzzydatetime_days_ago,
+                        timeDifference / DAYS,
+                        timeDifference / DAYS)
+                }
+                timeDifference < MONTHS -> {
+                    res.getQuantityString(R.plurals.fuzzydatetime_weeks_ago,
+                        timeDifference / WEEKS,
+                        timeDifference / WEEKS)
+                }
+                timeDifference < YEARS -> {
+                    res.getQuantityString(R.plurals.fuzzydatetime_months_ago,
+                        timeDifference / MONTHS,
+                        timeDifference / MONTHS)
+                }
+                else -> {
+                    res.getQuantityString(R.plurals.fuzzydatetime_years_ago,
+                        timeDifference / YEARS,
+                        timeDifference / YEARS)
+                }
+            }
+        } ?: run {
+            Log.e(TAG, "Context is null")
+            return date.toString()
+        }
     }
 }
